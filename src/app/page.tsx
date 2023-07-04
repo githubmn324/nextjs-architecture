c1982148517b844e180d6a39b34e61c4857bf7ba
05617 <sh.kimura@scsk.jp>
2023-07-04 18:01:42 +0900

    add tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,35 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import styles from './page.module.css';
-import { Button } from '../components/ButtonKimura';
+import { Button } from "../components/ButtonKimura";
 // import { Button } from '@/stories/Button';
 
 export default function Home() {
   const test = () => {
-    console.log('#####test');
+    console.log("#####test");
   };
   function test2() {
-    console.log('#####test2');
+    console.log("#####test2");
   }
 
   return (
     <>
-      <h1>テスト</h1>
+      <h1 className="text-3xl font-bold underline">テスト</h1>
+      <div>
+        <h1>テスト2</h1>
+      </div>
+
       <p>
-        <button type="button" onClick={test}>
+        <button
+          className="bg-gray-400 hover:bg-gray-300 text-white rounded px-4 py-2"
+          onClick={test}
+        >
           Delete Todo
         </button>
       </p>
-      <Button label="test" onClick={test}></Button>
+      <br></br>
+      <p>
+        <Button label="test" onClick={test2}></Button>
+      </p>
     </>
   );
   // return (
--- src/components/ButtonKimura.tsx
+++ src/components/ButtonKimura.tsx
@@ -2,12 +2,12 @@ interface ButtonProps {
   /**
    * label kimura!!!
    */
-  label: string
+  label: string;
   /**
    * Optional click handler
    */
   // onClick: (val:string) => void;
-  onClick?: () => void
+  onClick?: () => void;
 }
 
 // export const Button = ({label,onClick}: ButtonProps) => {
@@ -24,8 +24,15 @@ interface ButtonProps {
 // };
 
 export const Button = ({ label, onClick }: ButtonProps) => {
-  return <button onClick={onClick}>{label}</button>
-}
+  return (
+    <button
+      className="bg-green-600 hover:bg-green-500 text-white rounded px-4 py-2"
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
 
 // function Button({ children ,onClick}) {
 //   return <button type="button" onClick={onClick}>
